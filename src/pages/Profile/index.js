26f3e1bcf01c3b1de1f1ce02def588b3a3f6c131
victorlbernardes <victorlbernardes@gmail.com>
2024-04-16 20:35:10 -0300

    Tela de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,43 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-
-import * as Animatable from 'react-native-animatable'
-import { useNavigation } from '@react-navigation/native'
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Profile() {
-    const navigation = useNavigation();
     return (
         <View style={styles.container}>
-            <Text>Perfil</Text>
+            <View style={styles.profileInfo}>
+                <View style={styles.avatarContainer}>
+                    <Image
+                        source={require('../../assets/profile.png')} // Substitua por um path válido para a foto do usuário
+                        style={styles.avatar}
+                    />
+                    <TouchableOpacity style={styles.editIcon}>
+                        <Icon name="edit" size={20} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.fieldContainer}>
+                    <Text style={styles.label}>Nome: </Text>
+                    <Text style={styles.value}>Ademir</Text>
+                    <TouchableOpacity style={styles.editIcon}>
+                        <Icon name="edit" size={20} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.fieldContainer}>
+                    <Text style={styles.label}>Descrição:</Text>
+                    <Text style={styles.value}>Flamenguista e Nerd</Text>
+                    <TouchableOpacity style={styles.editIcon}>
+                        <Icon name="edit" size={20} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.logoutButton}>
+                <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.floatingButton}>
+                <Icon name="plus" size={30} color="white" />
+            </TouchableOpacity>
         </View>
+
     );
 }
 
@@ -17,33 +45,72 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        paddingHorizontal: 20,
+        paddingTop: 20,
     },
-    containerHeader: {
-        marginTop: '14%',
-        marginBottom: '8%',
-        paddingStart: '5%',
-    },
-    message: {
-        fontSize: 28,
+    avatarContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom:20,
+      },
+    avatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+      },
+    title: {
+        fontSize: 24,
         fontWeight: 'bold',
-        color: '#FFF'
     },
-    containerForm: {
-        backgroundColor: '#FFF',
+    profileInfo: {
+        marginTop: 20,
+    },
+    fieldContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    value: {
+        fontSize: 16,
+        color: '#666',
         flex: 1,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%',
+        marginRight: 10,
     },
-    tilte: {
-        fontSize: 20,
-        marginTop: 28,
+    editIcon: {
+        backgroundColor: '#FF7518',
+        borderRadius: 10,
+        padding: 10,
     },
-    input: {
-        borderBottomWidth: 1,
-        height: 40,
-        marginBottom: 12,
+    logoutButton: {
+        backgroundColor: '#FF7518',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 15,
+        borderRadius: 10,
+        marginTop: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
         fontSize: 16,
     },
-})
+    floatingButton: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+        backgroundColor: 'green',
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+});
